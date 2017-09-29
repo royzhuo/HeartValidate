3c145952b669d2a10cf7293308b46e57fecce926
royzhuo <731924600@qq.com>
2017-09-29 09:15:47 +0800

    java 主函数

---
+++ src/main/java/com/main/MainByJava.java
@@ -0,0 +1,12 @@
+package com.main;
+
+/**
+ * Created by roy.zhuo on 2017/9/29.
+ */
+
+
+public class MainByJava {
+    public static void main(String[] args) {
+        System.out.println("Main by Java");
+    }
+}
